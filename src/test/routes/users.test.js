3d20e7c8eb58dfe6ffdaec85c028a5c34f02c239
R B suraj <rbsuraj432@gmail.com>
2026-02-10 12:37:17 +0530

    sometimes users.test passed most times didn't, shows syntax error create role

--- src/test/routes/users.test.js
+++ src/test/routes/users.test.js
@@ -3,15 +3,67 @@ const buildApp = require('../../app');
 const UserRepo = require('../../repos/user-repo');
 const pool = require('../../pool');
 
-beforeAll(()=>{
-    return pool.connect({
+const {randomBytes} = require('crypto');
+const {default: migrate} = require('node-pg-migrate');
+const format = require('pg-format');
+
+beforeAll(async ()=>{
+    // randomly generating a role/user name to connect to PG as
+    // postgres role has to start with a letter, randomBytes returns letters and numbers
+    const roleName = 'a'+ randomBytes(4).toString('hex');
+
+    // connect to PG as usual
+    await pool.connect({
         host: 'localhost',
         port: 5432,
         database: 'socialnetwork2_test',
         user: 'postgres',
         password: 'postgresroot'
     })
-})
+
+    // create a new role with above generated role
+    await pool.query(`
+        CREATE ROLE ${roleName} WITH LOGIN PASSWORD ${roleName};    
+    `)
+
+    // create a schema with the same name 
+    await pool.query(`
+        CREATE SCHEMA ${roleName} AUTHORIZATION ${roleName};    
+    `)
+
+    // disconnect entirely from PG 
+    await pool.close();
+
+    // run migrations inside the new schema 
+    // tests run in parallel, and generally migrations are done one by one
+    // multiple tests running their migrations in parallel, this may error
+    // with noLock: true we're saying not to lock the db when migration is running
+    await migrate({
+        schema: roleName,
+        direction: 'up',
+        log: ()=>{},
+        noLock: true,
+        dir: 'migrations',
+        databaseUrl: {
+            host: 'localhost',
+            port: 5432,
+            database: 'socialnetwork2_test',
+            user: roleName,
+            password: roleName
+        }
+    })
+
+    // connect to PG as the newly created role  (same name as that of schema)
+    // so all the queries executed later will refer to the schema as that of user/role
+    await pool.connect({
+        host: 'localhost',
+        port: 5432,
+        database: 'socialnetwork2_test',
+        user: roleName,
+        password: roleName
+    });
+
+});
 
 afterAll(()=>{
     return pool.close();
